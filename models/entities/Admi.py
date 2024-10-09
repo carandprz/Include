fce0623c5794b5d21975e4faa5687daaf4a1f0c1
carandprz <carandprz1275@gmail.com>
2024-10-09 20:16:52 -0300

    Agrego Admi.py

---
+++ models/entities/Admi.py
@@ -0,0 +1,11 @@
+# models/entities/Admi.py
+from flask_login import UserMixin
+
+class Admi(UserMixin):
+    def __init__(self, username, password, name=None):
+        self.username = username
+        self.name = name
+        self.password = password
+
+    def get_id(self):
+        return str(self.username)  # Necesario para Flask-Login
