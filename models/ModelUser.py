e6f72d5c7094128d2c1f34a74d48134a1dde7058
carandprz <carandprz1275@gmail.com>
2024-10-10 10:57:16 -0300

    c

--- models/ModelUser.py
+++ models/ModelUser.py
@@ -1,19 +1,18 @@
-#metodo del usuario (user)
-from .entities.User import User
+from models.entities.User import User
 import database as db
 
 class ModelUser():
     
     @classmethod
-    def login(cls, db, user):
+    def login(cls, db_connection, user):
         try:
-            cursor = db.database.cursor()
+            cursor = db_connection.cursor()  # Usa 'db_connection' como se espera
             sql = "SELECT * FROM usuarios WHERE id_usuario = %s"
             cursor.execute(sql, (user.username,))
             row = cursor.fetchone()
             if row is not None:
-                user_db = User(row[0], row[4])
-                if user.password == user_db.password: # verifica contraseña
+                user_db = User(row[0], row[4], row[1], row[2])
+                if user.password == user_db.password:
                     return user_db
                 else:
                     return None
@@ -21,3 +20,19 @@ class ModelUser():
                 return None
         except Exception as ex:
             raise Exception(ex)
+    
+    @classmethod
+    def get_by_id(cls, db, id):
+        try:
+            cursor = db.database.cursor()  # Usa 'db.database' en lugar de 'db.connection'
+            sql = "SELECT id_usuario, nombre, apellido FROM usuarios WHERE id_usuario = %s"
+            cursor.execute(sql, (id,))
+            row = cursor.fetchone()
+            
+            if row is not None:
+                return User(row[0], None, row[1], row[2])
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
+
--- models/entities/User.py
+++ models/entities/User.py
@@ -1,10 +1,17 @@
 from werkzeug.security import check_password_hash   
+# models/entities/User.py
+from flask_login import UserMixin
 
-class User():
+class User(UserMixin):
     
-    def __init__(self,username,password):
+    def __init__(self, username, password, name=None, lastname=None):
         self.username = username
         self.password = password
+        self.name = name  # Almacena el nombre del usuario
+        self.lastname = lastname  # Almacena el apellido del usuario
+    
+    def get_id(self):
+        return str(self.username)  # Esto es necesario para Flask-Login
 
     #contraseña con hash
     #@classmethod
